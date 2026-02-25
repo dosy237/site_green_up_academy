e29b240909f521cf73ddb639b4428a44ff53d937
dosy237 <dosyca35@gmail.com>
2026-02-25 16:39:03 +0100

    docs: mise à jour

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-//export default defineConfig({
-  //plugins: [react()],
+//https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
   //server: {
     //proxy: {
       //'/api': {
@@ -11,6 +11,6 @@ import react from '@vitejs/plugin-react'
         //changeOrigin: true,
         //rewrite: (path) => path.replace(/^\/api/, '/api')
      // }
-   // }
+    //}
   //}
-//})
+})
